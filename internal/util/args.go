d58d46831cb99ed234c8655808003c2ab4611203
yoheinbb <yoheinbb.git@gmail.com>
2026-04-30 02:33:45 +0900

    Add RPM build flow and VERSION-based versioning

--- internal/util/args.go
+++ internal/util/args.go
@@ -9,6 +9,7 @@ type Args struct {
 	GlobalConfigFile *string
 	ScriptConfigFile *string
 	LogLevel         slog.Level
+	ShowVersion      bool
 }
 
 // CommandLineオプションの読み込み
@@ -17,8 +18,10 @@ func ReadCommandArgs() *Args {
 	cmdarg.GlobalConfigFile = flag.String("global-config", "conf/global.json", "global-config: json global-config for http port, url path.")
 	cmdarg.ScriptConfigFile = flag.String("script-config", "conf/script.json", "script-config: json sciprt config for script path and script exec settings.")
 	logLevel := flag.Bool("v", false, "v: verbose logs")
+	showVersion := flag.Bool("version", false, "print version and exit")
 	flag.Parse()
 
+	cmdarg.ShowVersion = *showVersion
 	if *logLevel {
 		cmdarg.LogLevel = slog.LevelDebug
 	} else {
